9364038e520ef06b969dc3d65b6f69b79776b964
leonardoamsilva <leoanjosmachadosilva@gmail.com>
2025-09-22 19:37:37 -0300

    ajustes

--- teste_csv.py
+++ teste_csv.py
@@ -1,28 +1,21 @@
-# teste_csv.py
 import pandas as pd
 
-# --- CONFIGURAÇÃO ---
-# Coloque o nome do seu arquivo CSV aqui
 file_csv = 'b3_acoes.csv' 
-# Coloque o nome provável da coluna com os tickers. Chute inicial: 'ticker' ou 'codigo'.
-# Vamos descobrir o nome certo logo abaixo.
-ticker = 'Ticker' # <--- VOCÊ VAI PRECISAR AJUSTAR ESTA LINHA
+
+ticker = 'Ticker'
 
 try:
-    # O Pandas lê o arquivo. Tente com sep=';' primeiro. Se não der certo, tente sep=','.
     df_acoes = pd.read_csv(file_csv, sep=';')
 
     print("--- Arquivo CSV lido com sucesso! ---")
     
-    # 1. Vamos ver as 5 primeiras linhas para entender a estrutura
+  
     print("\n--- Amostra do seu arquivo (5 primeiras linhas): ---")
     print(df_acoes.head())
     
-    # 2. Esta é a parte mais importante: vamos ver o nome de TODAS as colunas
     print("\n--- Nomes das colunas encontradas no arquivo: ---")
     print(df_acoes.columns.tolist())
     
-    # 3. Agora, tente extrair a coluna de tickers para uma lista
     print(f"\nTentando extrair a coluna chamada '{ticker}'...")
     lista_de_tickers = df_acoes[ticker].tolist()
     
