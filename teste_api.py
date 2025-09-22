b15d6af1310130111a95d56d587e348f9756229a
leonardoamsilva <leoanjosmachadosilva@gmail.com>
2025-09-22 19:38:37 -0300

    novos ajustes

--- teste_api.py
+++ teste_api.py
@@ -1,5 +1,3 @@
-# teste_api.py
-
 import requests
 import json
 
@@ -10,18 +8,15 @@ def explorar_api_tickers():
     
     try:
         response = requests.get(url)
-        response.raise_for_status() # Verifica se a requisição foi bem-sucedida
-        
-        # A MÁGICA: .json() converte a resposta JSON diretamente para um objeto Python (lista de dicionários)
+        response.raise_for_status() 
+
         dados = response.json()
         
         print(f"\nSucesso! A API retornou {len(dados)} tickers.")
         print("\n--- Amostra dos 3 primeiros itens retornados: ---")
-        # Usando json.dumps para imprimir de forma bonita (indentado)
+
         print(json.dumps(dados[:3], indent=2, ensure_ascii=False))
         
-        # Agora, vamos extrair apenas a lista de strings dos tickers
-        # Usando uma técnica chamada "List Comprehension" - uma forma 'pythônica' e eficiente de criar listas
         lista_de_tickers = [item['ticker'] for item in dados]
         
         print("\n--- Lista de tickers extraída (10 primeiros): ---")
@@ -33,6 +28,6 @@ def explorar_api_tickers():
         print(f"Erro ao acessar a API: {e}")
         return None
 
-# --- Vamos executar nossa função de exploração ---
+
 if __name__ == "__main__":
     explorar_api_tickers()
